b1da036f3cc9763df5eb57538460dd49873d2cee
Jesué Junior <jesue.junior@sieve.com.br>
2012-08-31 14:31:33 -0300

    Adicionando index.html

---
+++ src/utils/DoFile.java
@@ -0,0 +1,32 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DoFile {
+
+	public synchronized static void save(String file, String content, boolean add)
+			throws IOException {
+
+		FileWriter fw = new FileWriter(file, add);
+
+		fw.write(content);
+		fw.close();
+	}
+
+	public synchronized static String read(String filename) throws IOException {
+		BufferedReader inputStream = new BufferedReader(new FileReader(filename));
+		StringBuilder file = new StringBuilder();
+		String linha;
+		while ((linha = inputStream.readLine()) != null) {
+			file.append(linha);
+			
+		}
+		inputStream.close();
+		return file.toString(); 
+	}
+}
